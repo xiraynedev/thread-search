d4e945ec5976b6f5a09f2d936b9e443d22d8614b
xiraynedev <xiraynedev@gmail.com>
2022-10-27 17:02:31 -0400

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        public static void GetFileSizes(string directory)
+        {
+            // This method recursively gets the sizes of all files for a directory.
+            // If unauthorized access to a directory is detected, the method outputs a message and returns.
+
+            long totalSizeInBytes = 0;
+
+            if (Directory.Exists(directory))
+            {
+                try
+                {
+                    foreach (string file in Directory.EnumerateFiles(directory, "*", SearchOption.AllDirectories))
+                    {
+                        totalSizeInBytes += new FileInfo(file).Length;
+                    }
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+
+            Console.WriteLine($"The total size of the files in directory {Thread.CurrentThread.Name} is {totalSizeInBytes} bytes.");
+        }
+        public static void StartThreads(string[] directories)
+        {
+            // This method assigns each directory to a thread.
+            // Each thread calls the GetFileSizes method to retrieve the sizes of all files for each directory.
+
+            foreach (string directory in directories)
+            {
+                Thread thread = new Thread(() => GetFileSizes(directory))
+                {
+                    Name = directory
+                };
+                thread.Start();
+                // Using thr.Join() to wait for the threads to complete. It will lock the UI for about one second. Without using Join, the CPU time used is less than one second.
+                thread.Join();
+            }
+        }
+        private static void Main()
+        {
+            // The main method prompts users to enter up to three directories separated by spaces.
+            // If the letter q is detected, the program exits.
+            // It then validates the length of the directories.
+            // If validations pass, the method calls the StartThreads method with the directories passed to it.
+
+            bool quitProcess = false;
+
+            while (true)
+            {
+                string[] directories;
+
+                while (true)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(@"Enter up to three input directories separated by spaces. Enter Q to quit.");
+                    Console.Write(@"(e.g. c:\input c:\numbers c:\files): ");
+
+                    directories = Console.ReadLine().Trim().Split(' ');
+
+                    if (directories.Length == 1 && directories[0].ToLower() == "q")
+                    {
+                        quitProcess = true;
+                        break;
+                    }
+
+                    Console.WriteLine();
+
+                    if (directories.Length > 3)
+                    {
+                        Console.WriteLine("Please enter up to three directories only.");
+                        continue;
+                    }
+
+                    break;
+                }
+
+                if (quitProcess)
+                {
+                    break;
+                }
+
+                StartThreads(directories);
+            }
+        }
+    }
+}
+
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows;
+using System.Threading;
+using System.IO;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _ = InputDirectories.Focus();
+        }
+        public static void GetFileSizes(string directory, out long totalSizeInBytes)
+        {
+            // This method enumerates recursively to search file sizes in all directories and sub-directories.
+            // An output parameter is specified to output the total size of the files.
+            // Upon attempting to access an unauthorized directory, a message box will appear with the error
+            // message and output a file size of 0.
+
+            totalSizeInBytes = 0;
+
+            if (Directory.Exists(directory))
+            {
+                try
+                {
+                    foreach (string file in Directory.EnumerateFiles(directory, "*", SearchOption.AllDirectories))
+                    {
+                        totalSizeInBytes += new FileInfo(file).Length;
+                    }
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    _ = MessageBox.Show(e.Message, "Access Denied");
+                    totalSizeInBytes = 0;
+                }
+            }
+        }
+        public static string StartThreads(string[] directories)
+        {
+            // This method assigns each directory to a thread, calls the GetFileSizes method to recursively get
+            // the size of every file and get their total sum, and return the display text to the main method along
+            // with the total size of the files.
+
+            long totalSizeInBytes = 0;
+            string displayText = string.Empty;
+
+            foreach (string directory in directories)
+            {
+                Thread thread = new Thread(() => GetFileSizes(directory, out totalSizeInBytes))
+                {
+                    Name = directory
+                };
+
+                thread.Start();
+                thread.Join();
+
+                displayText += $"The total file size for {thread.Name} is {totalSizeInBytes} Bytes. \n";
+            }
+
+            return displayText;
+        }
+        public static bool ValidateDirectoryLength(string[] inputDirectories)
+        {
+            // This method validates the directories to make sure only up to three are provided.
+
+            bool isDirectoryLengthValid = true;
+
+            if (inputDirectories.Length > 3)
+            {
+                isDirectoryLengthValid = false;
+            }
+
+            return isDirectoryLengthValid;
+        }
+        private void BtnReadFileSizes(object sender, RoutedEventArgs e)
+        {
+            // The code has been split into separate methods to avoid having this method do all the tasks.
+            // This method gets the directories from the textbox, sends them to have their length evaluated, clears the
+            // previous textblock value, starts the threads with the directories, and displays the results in the textblock.
+            // If the length of the directories exceeds three, a message box is displayed and the method returns.
+
+            string[] directories = InputDirectories.Text.Trim().Split(' ');
+
+            if (!ValidateDirectoryLength(directories))
+            {
+                _ = MessageBox.Show("Please enter up to three directories only.", "Up To Three Directories Only");
+                return;
+            }
+
+            DisplayFileSizes.Text = "";
+
+            DisplayFileSizes.Text = StartThreads(directories);
+        }
+    }
+}
